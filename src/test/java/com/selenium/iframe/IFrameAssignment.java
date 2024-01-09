a06c0fc16164a3bef39a0866fc1e4591a9edfedc
Sebastian Lozano <juansebastian.lozano@publicissapient.com>
2024-01-09 11:00:34 -0500

    Nested iFrames completed

--- src/test/java/com/selenium/iframe/IFrameAssignment.java
+++ src/test/java/com/selenium/iframe/IFrameAssignment.java
@@ -1,4 +1,46 @@
 package com.selenium.iframe;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
 public class IFrameAssignment {
+
+    WebDriver driver;
+
+    WebDriverWait wait;
+
+    Actions actions;
+
+    @BeforeClass
+    public void setupDriver() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        actions = new Actions(driver);
+    }
+
+    @BeforeMethod
+    public void goToMainPage() {
+        driver.manage().window().maximize();
+        driver.get("https://the-internet.herokuapp.com/nested_frames");
+    }
+
+    @Test
+    public void withClickAndHoldMethod() {
+        driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='frame-top']")));
+        driver.switchTo().frame(driver.findElement(By.xpath("//frame[@name='frame-middle']")));
+        System.out.println(driver.findElement(By.id("content")).getText());
+    }
+
+    @AfterTest
+    public void tearDown() {
+        driver.quit();}
 }
